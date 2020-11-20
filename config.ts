ccc700f74fba17cdff33f3d5ef75d69824dc2d5f
RicoRi <3747377@qq.com>
2020-11-20 18:03:25 +0800

    feat: change gitignore

---
+++ config.ts
@@ -0,0 +1,40 @@
+const config = {
+
+  wsConfig: {
+    host: "127.0.0.1",
+    port: 6700,
+    enableAPI: true,
+    enableEvent: true,
+    access_token: "",
+    reconnection: true,
+    reconnectionAttempts: 8,
+    reconnectionDelay: 8000,
+  },
+
+  yoruConfig: {
+    admin: [515302066],       //管理员qq数组
+    autoAddFriend: false,     //自动添加好友
+    autoAddGroup: false,      //自动进群
+
+    searchLimit: 30,
+    textMode: false,
+    repeater: {
+      enable: true,
+      times: 3
+    },
+    setu: {
+      enable: true,
+      allowPM: true,
+      cd: 5,
+      deleteTime: 0,
+      limit: 30,
+      whiteGroup: [],
+      whiteOnly: false,
+      whiteCd: 5,
+      whiteDeleteTime: 60
+    }
+  },
+
+}
+
+export default config;
