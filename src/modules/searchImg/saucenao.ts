06f31c5df74685cb9724d2f8959d7d45d080c642
RicoRi <3747377@qq.com>
2021-06-02 18:18:32 +0800

    feat: 搜图模块改进

--- src/modules/searchImg/saucenao.ts
+++ src/modules/searchImg/saucenao.ts
@@ -1,14 +1,6 @@
 import Axios from 'axios';
 import MessageCode from '../../core/MessageCode';
 
-const hosts = ["saucenao.com"];
-interface ISnDB {
-  all: 999,
-  pixiv: 5,
-  danbooru: 9,
-  book: 18,
-  anime: 21
-}
 
 /**
  * saucenao搜索
@@ -16,102 +8,141 @@ interface ISnDB {
  * @param {string} imgURL 图片地址
  */
 export default async function saucenaoSearch(imgURL: string) {
-  const ret = await getSearchResult(hosts[0], imgURL, 999);
-  if (ret.results && ret.results.length > 0) {
-    const { data, header } = ret.results[0];
-    //console.log(ret.results[0]);
-    const {
-      similarity, //相似度
-      thumbnail //缩略图
-    } = header;
-    let {
-      title, //标题
-      member_name, //作者
-      jp_name //本子名
-    } = data;
 
-    let isAnime = false;
-    let isBook = false;
-
-    const extUrls = data.ext_urls || [];
-    let url = extUrls[0];
-    //如果结果有多个，优先取danbooru
-    extUrls.forEach((u: string) => {
-      if (u.indexOf('danbooru') !== -1) {
-        url = u;
-      }
-    })
-    url = url.replace('http://', 'https://');
-    //处理pixiv替换
-    let pidSearch = /pixiv.+illust_id=([0-9]+)/.exec(url);
-    if (pidSearch) {
-      url = 'https://pixiv.net/i/' + pidSearch[1];
-    }
-    const origURL = url.replace('https://', '');
-    isAnime = origURL.indexOf("anidb.net") !== -1;
-    if (jp_name && jp_name.length > 0) {
-      isBook = true;
-    }
-    //标题处理
-    if (!title) {
-      title = isAnime ? '[AniDB]' : '[YoruDB]';
-    }
-    if (member_name && member_name.length > 0) {
-      title = `「${title}」/「${member_name}」`;
-    }
-
-    //低相似度警告
-    /*
-    if (similarity < 50) {
-      //warnMsg += CQ.escape(replyText.serchSimilarityLow);
-    }
-    */
-
-    //生成消息文本
-    const msg = MessageCode.share(url, `${title}\n相似度达到了${similarity}%`, origURL, thumbnail);
-
-    return {
-      success: true,
-      msg,
-      isAnime,
-      isBook,
-      details: {
-        similarity,
-        jp_name,
-        origURL,
-        thumbnail
+  // API请求与处理
+  let res;
+  try {
+    const ret = (await saucenaoFetch(imgURL)).data;
+    if (ret.results && ret.results.length > 0) {
+      res = ret.results[0] || {};
+    } else {
+      console.error(`${new Date().toLocaleString()} [Saucenao Error]Saucenao API Error`);
+      console.log(ret);
+      return {
+        success: false,
+        msg: ''
       }
     }
-  } else {
-    console.error(`${new Date().toLocaleString()} [Saucenao Error]API ERROR`);
+  } catch (error) {
+    console.error(`${new Date().toLocaleString()} [Saucenao Error]SaucenaoFetch Error`);
+    console.log(error);
     return {
       success: false,
       msg: ''
     }
   }
+
+  // 解析对象结构
+  const {
+    header: {
+      similarity,
+      thumbnail
+    } = {},
+    data: {
+      ext_urls: extUrls = [],
+      title,
+      pixiv_id: pixivId,
+      member_name,
+      author_name,
+      jp_name
+    } = {}
+  } = res;
+
+  let isAnime = false, isBook = false;
+  let url = extUrls[0] || '', origURL = '';
+
+  // url处理
+  if (pixivId) {
+    url = 'https://pixiv.net/i/' + pixivId;
+  } else {
+    // 如果url有多个，优先取danbooru的
+    if (extUrls.length > 0) {
+      for (const u of extUrls) {
+        if (u.indexOf('danbooru') !== -1) {
+          url = u;
+          break;
+        }
+      }
+    }
+    const pidRegExpRes = /pixiv.+illust_id=([0-9]+)/.exec(url);
+    if (pidRegExpRes) {
+      url = 'https://pixiv.net/i/' + pidRegExpRes[1];
+    }
+    url = url.replace('http://', 'https://');
+    origURL = url.replace('https://', '');
+  }
+
+  // 结果类型判断
+  isAnime = origURL.indexOf("anidb.net") !== -1;
+  if (jp_name && jp_name.length > 0) {
+    isBook = true;
+  }
+
+  // 标题处理
+  let displayTitle = '';
+  if (!title) {
+    displayTitle = isAnime ? '[AniDB]' : '[YoruDB]';
+  }
+  if (member_name || author_name) {
+    displayTitle = `「${title}」/「${member_name || author_name}」`;
+  }
+
+  //生成消息文本
+  const msg = MessageCode.share(url, `${displayTitle}\n相似度达到了${similarity}%`, origURL, thumbnail || '');
+  return {
+    success: true,
+    msg,
+    isAnime,
+    isBook,
+    details: {
+      similarity,
+      jp_name,
+      origURL,
+      thumbnail
+    }
+  }
+}
+
+
+enum SnDBEnum {
+  ALL = 999,
+  PIXIV = 5,
+  DANBOORU = 9,
+  BOOK = 18,
+  ANIME = 21
+}
+
+interface ISaucenaoResult {
+  header: any,
+  results: {
+    header: {
+      similarity: string,
+      thumbnail: string,
+      index_name: string
+    },
+    data: {
+      ext_urls: string[],
+      title: string,
+      pixiv_id: number,
+      member_name: string,
+      author_name: string,
+      jp_name: string
+    }
+  }[]
 }
 
 /**
- * 取得搜图结果
- *
- * @param {*} host 自定义saucenao的host
- * @param {*} imgURL 欲搜索的图片链接
- * @param {number} [db=999] 搜索库
- * @returns Axios对象
+ * saucenao请求
  */
-async function getSearchResult(host: string, imgURL: string, db = 999) {
-  const res = await Axios.get('https://' + host + '/search.php', {
+function saucenaoFetch(imgURL: string) {
+  return Axios.get<ISaucenaoResult>('https://saucenao.com/search.php', {
     params: {
       api_key: '16abeee27bd15d00da11a60c92e7429321b8284e',
-      db: db,
-      output_type: 2,
-      numres: 3,
+      db: SnDBEnum.ALL,    // 搜索的DB
+      output_type: 2,  // API返回方式，2=JSON
+      numres: 3,   // 结果数量
       url: imgURL
     }
   });
-  if (res.data) {
-    return res.data
-  } else {
-    return {}
-  }
 }
+
