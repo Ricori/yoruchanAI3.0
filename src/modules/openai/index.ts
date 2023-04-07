a6a2df7768d02d5bae0c7cbe032dbcbf41d8dc75
Takamichika <515302066@qq.com>
2023-04-08 03:33:16 +0800

    feat: update proxy

--- src/modules/openai/index.ts
+++ src/modules/openai/index.ts
@@ -5,7 +5,7 @@ import YData from '../../core/yData';
 
 const configuration = new Configuration({
   apiKey: yoruConfig.openAi.apiKey,
-  basePath: 'https://openapi.ssiic.com/v1',
+  basePath: 'https://closeai.deno.dev/v1',
 });
 
 const openai = new OpenAIApi(configuration);
