e66d2be036bb184db91c51d9c84d5018563d0e82
Takamichika <515302066@qq.com>
2025-11-29 23:52:59 +0900

    Update admin.ts

--- src/modules/admin/admin.ts
+++ src/modules/admin/admin.ts
@@ -2,6 +2,7 @@ import { GroupMessageData, PrivateMessageData } from "@/types/event";
 import YoruModuleBase from "../base";
 import yorubot from '@/core/yoruBot';
 import { changeModel } from "@/service/ai";
+import { createMsgFromTweetId } from "@/tasks/twitter";
 
 export default class AdminModule extends YoruModuleBase<PrivateMessageData | GroupMessageData> {
 
@@ -16,11 +17,16 @@ export default class AdminModule extends YoruModuleBase<PrivateMessageData | Gro
       // Exec administrator command
 
       // AI model switch (chatgpt or deepseek)
-      const exec = /--ai_model=([^\s]+)/.exec(message);
-      if (exec !== null) {
+      const modelExec = /--ai_model=([^\s]+)/.exec(message);
+      if (modelExec !== null) {
         return true;
       }
 
+      // Push twiiter
+      const pushTwiiterExec = /--push-twiiter=(\d+)/.exec(message);
+      if (pushTwiiterExec !== null) {
+        return true;
+      }
 
     }
     return false;
@@ -33,11 +39,23 @@ export default class AdminModule extends YoruModuleBase<PrivateMessageData | Gro
     // AI model switch
     const match = message.match(/--ai_model=([^\s]+)/);
     const model = match ? match[1] : null;
-
     if (model === 'chatgpt' || model === 'deepseek') {
       changeModel(model);
       const reply = `[YoruSystem] The AI model successfully switched to ${model}.`;
       yorubot.sendMsg(groupId, userId, reply);
+    }
+
+    // Push twiiter
+    const tweetIdMatch = message.match(/--push-twiiter=(\d+)/);
+    const groupMatch = message.match(/--group=(\d+)/);
+    const tweetId = tweetIdMatch ? tweetIdMatch[1] : null;
+    const targetGroupId = groupMatch ? groupMatch[1] : null;
+    if (tweetId && targetGroupId) {
+      const msg = await createMsgFromTweetId(tweetId);
+      yorubot.sendMsg(Number(targetGroupId), undefined, msg);
+      yorubot.sendMsg(groupId, userId, `[YoruSystem] Push ${tweetId} to ${targetGroupId} successed.`);
+    } else {
+      yorubot.sendMsg(groupId, userId, '[YoruSystem] Push failed. Missing parameters.');
     }
 
     // finish
