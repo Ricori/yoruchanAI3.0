392aa6f525c5f125c97a7764fb2459c6e84fd880
Takamichika <515302066@qq.com>
2026-03-08 01:13:28 +0900

    Update clean.ts

--- src/tasks/clean.ts
+++ src/tasks/clean.ts
@@ -2,13 +2,11 @@ import { SimpleIntervalJob, Task } from 'toad-scheduler';
 import yoruStorage from '@/core/yoruStorage';
 
 const task = new Task('systemCleanupTask', () => {
-
   // 清理qq群会话缓存
   yoruStorage.cleanGroupChatConversations();
+});
 
-})
-
-const SystemCleanupJob = new SimpleIntervalJob({ days: 1, }, task, { id: 'systemCleanup' })
+const SystemCleanupJob = new SimpleIntervalJob({ days: 1 }, task, { id: 'systemCleanup' });
 
 
 export default SystemCleanupJob;
